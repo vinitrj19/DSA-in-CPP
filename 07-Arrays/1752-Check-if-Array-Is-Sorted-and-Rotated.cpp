2f96d9d560fdd27b5f9a8bdd3639ba86f6109e8f
Vinit Raj <vinitrj19@gmail.com>
2026-09-14 23:03:44 +0530

    Add solution for checking sorted and rotated array
    Implement a solution to check if an array is sorted and rotated based on the problem statement from LeetCode.

---
+++ 07-Arrays/1752-Check-if-Array-Is-Sorted-and-Rotated.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem Name: 1752. Check if Array Is Sorted and Rotated
+ * Source: LeetCode
+ * Difficulty: Easy
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+};
